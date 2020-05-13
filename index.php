bfeb060124a219e187d3342ec0086b3ce6aba4eb
cristilacatus <64159755+cristilacatus@users.noreply.github.com>
2020-05-13 07:59:08 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,10 @@
 </head>
 <body>
 <?php
-    $bgcolour = mt_rand( 0, 0xFFFFFF);
     $servername = gethostname();
     
-    echo "<body style='background-color:$bgcolour'>";
     echo "<b>This is a simple web application</b>\n";
+    echo "Version 1.0\n";
     echo "Server name $servername\n";
 ?>
 </body>
