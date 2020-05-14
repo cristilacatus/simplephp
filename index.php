05142635a90cdf2d0ef6caf019f1aafbf7b0af42
cristilacatus <64159755+cristilacatus@users.noreply.github.com>
2020-05-14 10:26:51 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
 <?php
     $servername = gethostname();
     
-    echo "<b>This is a simple web application</b>\n";
-    echo "Version 1\n";
-    echo "Server name $servername\n";
+    echo "<b>This is a simple web application</b>\r\n";
+    echo "Version 1\r\n";
+    echo "Server name $servername\r\n";
 ?>
 </body>
 </html>
